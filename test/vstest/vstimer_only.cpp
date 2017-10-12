796bd487f98b3ba3b6d1da84d38e181e9225fe8c
GorNishanov <GorNishanov@gmail.com>
2017-10-12 12:06:24 -0700

    added dispatch and post

--- test/vstest/vstimer_only.cpp
+++ test/vstest/vstimer_only.cpp
@@ -144,7 +144,8 @@ int main() {
   //udp_socket_test<io_context>("io_context", [](auto& io) { run(io, 8); });
   //udp_socket_test<tp_context>("tp_context", [](auto& io) { io.join(); });
   //udp_socket_test<io_context>("io_context", [](auto& io) { run(io, 8); });
-  tcp_socket_test<io_context>("io_context", [](auto& io) { run(io, 8); });
+  //tcp_socket_test<io_context>("io_context", [](auto& io) { run(io, 8); });
+  tcp_socket_test<tp_context>("tp_context", [](auto& io) { io.join(); });
   //post_test<io_context>("io_context", [](auto& io) { run(io, 8); });
   //post_test<tp_context>("tp_context", [](auto& io) { io.join(); });
   //timer_test<io_context>("io_context", [](auto& io) { run(io, 8);  });
