ccfb1b187293b76d8d848c29a67f8cb310cfdf43
GorNishanov <GorNishanov@gmail.com>
2017-10-12 21:08:10 -0700

    added simple intrusive list

--- include/experimental/__net_ts/detail/cancellable_object_owner.hpp
+++ include/experimental/__net_ts/detail/cancellable_object_owner.hpp
@@ -8,15 +8,18 @@
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 
-#ifndef NET_TS_DETAIL_OBJECT_POOL_HPP
-#define NET_TS_DETAIL_OBJECT_POOL_HPP
+#ifndef NET_TS_DETAIL_CANCELLABLE_OBJECT_OWNER_HPP
+#define NET_TS_DETAIL_CANCELLABLE_OBJECT_OWNER_HPP
 
 #if defined(_MSC_VER) && (_MSC_VER >= 1200)
 # pragma once
 #endif // defined(_MSC_VER) && (_MSC_VER >= 1200)
 
+#include <atomic>
+
 #include <experimental/__net_ts/detail/noncopyable.hpp>
 #include <experimental/__net_ts/detail/object_pool.hpp>
+#include <experimental/__net_ts/detail/wintp_mutex.hpp>
 
 #include <experimental/__net_ts/detail/push_options.hpp>
 
@@ -26,9 +29,118 @@ namespace net {
 inline namespace v1 {
 namespace detail {
 
-  struct cancellable_object_base {};
+// TODO: integrate with object_pool
 
-  struct cancellabl_object_owner {};
+struct cancellable_object_base {
+  friend class object_pool_access;
+
+  virtual ~cancellable_object_base() {}
+
+  bool try_set_cancel_pending(char const* name) {
+    auto oldValue =
+        state.fetch_and(~NOT_BEING_CANCELLED, std::memory_order_acq_rel);
+    bool firstToCancel = (oldValue & NOT_BEING_CANCELLED) != 0;
+    printf("%s: first-to-cancel %d\n", name, firstToCancel);
+    return firstToCancel;
+  }
+
+  bool try_increment_local_io() {
+    auto old = state.load(std::memory_order_relaxed);
+    auto newValue = old + REF;
+    do {
+      if ((old & NOT_BEING_CANCELLED) == 0)
+        return false;
+      newValue = old + REF;
+    } while (state.compare_exchange_weak(
+      old, newValue, std::memory_order_release, std::memory_order_relaxed));
+    return true;
+  }
+
+  void increment_local_io() {
+    if (!try_increment_local_io())
+      throw std::logic_error("tp_object is being canceled");
+  }
+
+  // returns true if no more refs
+  bool drop_ref_internal(char const* name) {
+    auto newValue = state.fetch_sub(REF, std::memory_order_acq_rel) - REF;
+    printf("%s: deref => %d\n", name, newValue / REF);
+    return (newValue == 0);
+  }
+
+  template <typename Object>
+  void drop_ref(char const* name, object_pool<Object>& pool) {
+    if (drop_ref_internal(name)) {
+      pool.free(static_cast<Object*>(this));
+    }
+  }
+
+private:
+  enum : unsigned {
+    NOT_BEING_CANCELLED = 1,
+    REF = 2,
+  };
+
+  cancellable_object_base *next = nullptr;
+  cancellable_object_base *prev = nullptr;
+
+  // Initially have two references. One for the handle to an object,
+  // another for being a member of the children list in the owner.
+  std::atomic<unsigned> state = NOT_BEING_CANCELLED + 2 * REF;
+};
+
+template <typename Object>
+struct cancellabl_object_owner
+{
+  void ParentCancel() {
+    auto itemsToCancel = extractItemsThatNeedCancelling();
+    while (T* item = itemsToCancel.try_pop()) {
+      item->InitiateCancel();
+      item->DropRef("parent");
+    }
+  }
+
+  void ChildAdd(T* item) noexcept {
+    bool needCancel = false;
+    {
+      std::lock_guard<std::mutex> grab(lock);
+      if (cancelRequested)
+        needCancel = true;
+      else
+        list.push_back(item);
+    }
+    if (needCancel) {
+      item->InitiateCancel();
+      item->DropRef("owner");
+    }
+  }
+
+  void ChildRemove(T* item) {
+    {
+      std::lock_guard<std::mutex> grab(lock);
+      item->erase_and_check_if_list_is_empty();
+    }
+    item->DropRef("owner", pool);
+  }
+
+  ~cancellabl_object_owner() { puts("OwnerOf: dtor"); }
+
+private:
+  intrusive_stack<T> extractItemsThatNeedCancelling() {
+    std::lock_guard<std::mutex> grab(lock);
+    if (cancelRequested)
+      return {};
+    cancelRequested = true;
+    return list.extract_if(
+      [](auto *item) { return item->TrySetCancelPending("paren"); });
+  }
+
+private:
+  wintp_mutex lock;
+  bool cancelRequested = false;
+
+  object_pool<Object> pool;
+};
 
 } // namespace detail
 } // inline namespace v1
---
+++ include/experimental/__net_ts/detail/simple_intrusive_list.hpp
@@ -0,0 +1,175 @@
+//
+// detail/simple_intrusive_list.hpp
+// ~~~~~~~~~~~~~~~~~~~~~~~~
+//
+// Copyright (c) 2017 No copyright claimed
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#ifndef NET_TS_DETAIL_SIMPLE_INTRUSIVE_LIST_HPP
+#define NET_TS_DETAIL_SIMPLE_INTRUSIVE_LIST_HPP
+
+// FIXME: reconcile with object_pool
+
+#include <cassert>
+
+struct simple_intrusive_list_base {
+  struct entry {
+    entry *next;
+    entry *prev;
+
+    bool erase_and_check_if_list_is_empty() {
+      entry *const prev_entry = this->prev;
+      entry *const next_entry = this->next;
+      prev_entry->next = next_entry;
+      next_entry->prev = prev_entry;
+      return next_entry == prev_entry;
+    }
+  };
+  entry head;
+
+  simple_intrusive_list_base(simple_intrusive_list_base const &) = delete;
+  simple_intrusive_list_base(simple_intrusive_list_base &&) = delete;
+
+  simple_intrusive_list_base() { clear(); }
+  bool empty() const { return head.next == &head; }
+
+  void clear() { head.next = head.prev = &head; }
+  bool is_head(entry *e) const { return e == &head; }
+
+  entry *pop_front() {
+    entry *const front = head.next;
+    entry *const next = front->next;
+    head.next = next;
+    next->prev = &head;
+    return front;
+  }
+
+  entry *pop_back() {
+    entry *const back = head.prev;
+    entry *const prev = back->prev;
+    head.prev = prev;
+    prev->next = &head;
+    return back;
+  }
+
+  void push_back(entry *new_entry) {
+    entry *const prev = head.prev;
+    new_entry->next = &head;
+    new_entry->prev = prev;
+    prev->next = new_entry;
+    head.prev = new_entry;
+  }
+
+  void push_front(entry *new_entry) {
+    entry *const next = head.next;
+    new_entry->next = next;
+    new_entry->prev = &head;
+    next->prev = new_entry;
+    head.next = new_entry;
+  }
+};
+
+using simple_intrusive_list_entry = simple_intrusive_list_base::entry;
+
+template <typename T> struct simple_intrusive_stack {
+  simple_intrusive_list_entry *root = nullptr;
+
+  simple_intrusive_stack() = default;
+  simple_intrusive_stack(simple_intrusive_stack const &) = delete;
+  simple_intrusive_stack(simple_intrusive_stack &&rhs) : root(rhs.root) {
+    rhs.root = nullptr;
+  }
+  simple_intrusive_stack &operator=(simple_intrusive_stack const &) = delete;
+  simple_intrusive_stack &operator=(simple_intrusive_stack &&rhs) {
+    if (std::addressof(rhs) != this) {
+      assert(root == nullptr);
+      root = rhs.root;
+      rhs.root = nullptr;
+    }
+    return *this;
+  }
+
+  ~simple_intrusive_stack() { assert(root == nullptr); }
+
+  void push(T *x) {
+    x->next = root;
+    root = x;
+  }
+
+  T *try_pop() {
+    if (auto result = root) {
+      root = result->next;
+      return static_cast<T *>(result);
+    }
+    return nullptr;
+  }
+};
+
+template <typename T>
+struct simple_intrusive_list : public simple_intrusive_list_base {
+  using list_base = simple_intrusive_list_base;
+  T *pop_front() {
+    auto e = list_base::pop_front();
+    return static_cast<T *>(e);
+  }
+  T *pop_back() {
+    auto e = list_base::pop_back();
+    return static_cast<T *>(e);
+  }
+  T *pop_front_or_null() {
+    auto e = list_base::pop_front();
+    if (is_head(e))
+      return nullptr;
+    return static_cast<T *>(e);
+  }
+  T *pop_back_or_null() {
+    entry *e = list_base::pop_back();
+    if (is_head(e))
+      return nullptr;
+    return static_cast<T *>(e);
+  }
+
+  template <typename Pred> simple_intrusive_stack<T> extract_if(Pred p) {
+    simple_intrusive_stack<T> result;
+    auto *current = head.next;
+    while (current != &head) {
+      auto *next_item = current->next;
+      T *value = static_cast<T *>(current);
+      if (p(value)) {
+        value->erase_and_check_if_list_is_empty();
+        result.push(value);
+      }
+      current = next_item;
+    }
+    return result;
+  }
+
+  ~simple_intrusive_list() {
+    assert(empty() && "expect list to be empty on destruction");
+  }
+
+  struct iterator {
+    entry *e;
+
+    T *operator*() { return static_cast<T *>(e); }
+    iterator operator++() {
+      e = e->next;
+      return *this;
+    }
+    iterator operator++(int) {
+      iterator result{e};
+      e = e->next;
+      return result;
+    }
+    bool operator!=(iterator rhs) { return e != rhs.e; }
+    bool operator==(iterator rhs) { return e == rhs.e; }
+  };
+
+  iterator begin() { return iterator{head.next}; }
+  iterator end() { return iterator{&head}; }
+};
+
+#endif
